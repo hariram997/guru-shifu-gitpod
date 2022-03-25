08d518c825ed75d1269f3b3d2d67d5de6cc20f86
hariramprasanth balasubramanian <hariramprasanth.b@thoughtworks.com>
2022-03-25 09:50:49 +0530

    #785 | Debug. Paused frontend image build | [Hari/mathu]

--- .ignore/start.sh
+++ .ignore/start.sh
@@ -10,15 +10,15 @@ touch .env
 echo "REACT_APP_HOST_URL=https://8080-${GITPOD_WORKSPACE_URL#*//}" > .env
 echo "$(timestamp) Starting guru-shifu..."
 echo "$(timestamp) Running docker-compose up in detach mode.."
-docker-compose -f docker-compose-gitpod.yml up -d --quiet-pull
+docker-compose -f docker-compose-gitpod.yml up -d
 echo "$(timestamp) Docker compose completed."
-if [ $? == 0 ]
-then
-  echo "$(timestamp) Waiting for guru-shifu to start up.... "
-  until $(curl --output /dev/null --silent --head --fail http://localhost:3000/); do
-    printf "."
-    sleep 1
-  done
-  echo "$(timestamp) Guru-shifu started successfully..."
-fi
+# if [ $? == 0 ]
+# then
+#   echo "$(timestamp) Waiting for guru-shifu to start up.... "
+#   until $(curl --output /dev/null --silent --head --fail http://localhost:3000/); do
+#     printf "."
+#     sleep 1
+#   done
+#   echo "$(timestamp) Guru-shifu started successfully..."
+# fi
 cd /workspace
